729ebce292458c26e88a3126a4bda00088aeeac7
nickrallison <nickrallison1@gmail.com>
2023-02-02 12:05:34 -0700

    regex go brrrr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::fs;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    // Writing File
+    let mut path: String = "src/".to_owned();
+    let file_name: String = "foo.tex".to_owned();
+    path.push_str(&file_name);
+    let data = "Some data!";
+    fs::write(path, data).expect("Unable to write file");
+
+    // Questions Input
+    let input_file_path = "text/q.txt";
+    let contents = fs::read_to_string(input_file_path)
+        .expect("Should have been able to read the question input text file");
+
+
+    // Finds (Start): (End)
+    let rx = Regex::new(r"^(?P<key>([^:]+))[:][ ](?P<val>([^:]+))$").unwrap();
+    for line in contents.lines() {
+        
+	    if let Some(c) = rx.captures(line) {
+	        println!("Key: {}, Val = {}", &c["key"], &c["val"]);
+	    }
+        else {
+            println!("~~~~~~");
+        }
+	}
+
 }
+
+
+/* Saved Regex's
+ * --------------
+ * ^(?P<key>([^:]+))[:][ ](?P<val>([^:]+))$     -- Saves each key value pair
+ * (?P<text>([^~]+))[~]                         -- Saves each text block
+ */
