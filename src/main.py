d20f440de1dc4573dbeb70c1409370d18a94704d
nickrallison <nickrallison1.gmail.com>
2023-03-17 07:09:27 -0600

    adding boiler plate

--- src/main.py
+++ src/main.py
@@ -4,6 +4,15 @@ def main(debugging=False):
     doc = TexDocument("text", "latex")
     doc.get_questions("PHYS 259", "question_bank.json", ["Coulomb's Law"], [3])
     doc.print_tex("out.tex")
+
+    print_cmd = "pdflatex -output-directory=latex  latex/out.tex"
+    returned_value = os.system(cmd)  # returns the exit code in unix
+    print('returned value:', returned_value)
+
+    if (not debugging):
+        pass
+    else:
+        pass
     pass
 
 if __name__ == '__main__':
