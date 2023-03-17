4cedc6996440eac9ed9179c4ab45278cb578dbc0
nickrallison <nickrallison1.gmail.com>
2023-03-17 07:13:42 -0600

    python can run latex print

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from tex import TexDocument
+import os
 
 def main(debugging=False):
     doc = TexDocument("text", "latex")
@@ -6,13 +7,15 @@ def main(debugging=False):
     doc.print_tex("out.tex")
 
     print_cmd = "pdflatex -output-directory=latex  latex/out.tex"
-    returned_value = os.system(cmd)  # returns the exit code in unix
-    print('returned value:', returned_value)
+    returned_value_print = os.system(print_cmd)  # returns the exit code in unix
 
-    if (not debugging):
-        pass
-    else:
-        pass
+    #if (not debugging):
+    clean_cmd = "rm -rf latex/*.log && rm -rf latex/*.aux && rm -rf latex/out.tex"
+    returned_value_clean = os.system(clean_cmd)  # returns the exit code in unix
+
+    if debugging:
+        print('exit code for latex print:', returned_value_print)
+        print('exit code for latex clean:', returned_value_clean)
     pass
 
 if __name__ == '__main__':
